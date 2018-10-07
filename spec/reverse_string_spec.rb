583c16992914f406b0ac33e23ab7eb6e171d2938
thecodenomad@gmail.com <thecodenomad@gmail.com>
2018-10-07 21:17:18 +0800

    Added specs for both reverse string methods

--- spec/reverse_string_spec.rb
+++ spec/reverse_string_spec.rb
@@ -1,7 +1,12 @@
 require 'reverse_string'
 
 RSpec.describe ReverseString do
-  it 'reverses a string' do
+  it 'reverses a string using the #reverse method' do
+    word = ReverseString.new('Dictionary')
+    expect(word.reverse).to eq 'yranoitciD'
+  end
+
+  it 'reverses a string using the #reversex method' do
     word = ReverseString.new('Dictionary')
     expect(word.reversex).to eq 'yranoitciD'
   end
