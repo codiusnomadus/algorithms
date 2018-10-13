0f6dba6ef9252587453f5f642e557fff1b9c428f
thecodenomad@gmail.com <thecodenomad@gmail.com>
2018-10-13 16:28:58 +0800

    Cleaned reverse method in reverse_integer.rb

--- lib/reverse_integer.rb
+++ lib/reverse_integer.rb
@@ -7,7 +7,6 @@ class ReverseInteger
 
   # Method 1: Using the reverse method
   def reverse
-    reversed_num = num.to_s.reverse.to_i
-    reversed_num * (num <=> 0)
+    num.to_s.reverse.to_i * (num <=> 0)
   end
 end
